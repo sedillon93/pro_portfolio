78c4e13141ce5553e485e11f150c30433580f9b0
Shannon Dillon <sedillon93@gmail.com>
2017-10-10 15:45:55 -0700

    fill out more of project template; add Project constructor

--- public/scripts/controller/projectController.js
+++ public/scripts/controller/projectController.js
@@ -1,7 +1,13 @@
 'use strict';
 
-(function(){
-  let projectTemplateHTML = $('#projectTemplate').html();
-  let compiledProjectTemplate = Handlebars.compile(projectTemplateHTML);
-  compiledProjectTemplate
-})()
+function Project(title, description, languages, dates, link){
+  this.title = title,
+  this.description = description,
+  this.languages = languages,
+  this.dates = dates,
+  this.link = link
+}
+
+let projectTemplateHTML = $('#projectTemplate').html();
+let compiledProjectTemplate = Handlebars.compile(projectTemplateHTML);
+compiledProjectTemplate
--- public/scripts/model/projects.js
+++ public/scripts/model/projects.js
@@ -1,5 +1,22 @@
-'use strict';
-
-function Project(title){
-  title: this.title
-}
+[
+  {
+    title: 'Report My Wreck',
+    languages: 'JavaScript, CSS, HTML',
+    dates: 'October 2017'
+  },
+  {
+    title: 'Scramble',
+    languages: 'JavaScript, CSS, HTML',
+    dates: 'August 2017'
+  },
+  {
+    title: 'Salmon Cookies',
+    languages: 'JavaScript, CSS, HTML',
+    dates: 'August 2017'
+  },
+  {
+    title: 'Bus Mall',
+    languages: 'JavaScript, CSS, HTML',
+    dates: 'August 2017'
+  }
+]
