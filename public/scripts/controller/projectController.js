4937639d5e3b5ae51d3a2559b1608cb39723dd47
Shannon Dillon <sedillon93@gmail.com>
2017-10-10 08:00:24 -0700

    trying to hook up handlebars template

---
+++ public/scripts/controller/projectController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function(){
+  let projectTemplateHTML = $('#projectTemplate').html();
+  let compiledProjectTemplate = Handlebars.compile(projectTemplateHTML);
+  compiledProjectTemplate
+})()
