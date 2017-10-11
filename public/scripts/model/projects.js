c2a23dafbb9c16ca44997df8d1a8d30efbe690d7
Shannon Dillon <sedillon93@gmail.com>
2017-10-11 08:26:14 -0700

    trying to fix issue with projects.json not connecting, realize server.js isn't set up yet

--- public/scripts/model/projects.js
+++ public/scripts/model/projects.js
@@ -1,5 +1,35 @@
 'use strict';
 
-$.get('data/projects.json', function(){
-  createProjects();
-})
+const projects = [];
+
+function Project(projectData){
+  this.title = projectData.title,
+  this.description = projectData.description,
+  this.languages = projectData.languages,
+  this.dates = projectData.dates,
+  this.link = projectData.link
+}
+
+Project.prototype.toHtml = function(){
+  let projectTemplateHTML = $('#projectTemplate').html();
+  let compiledProjectTemplate = Handlebars.compile(projectTemplateHTML);
+  return filledProjectTemplate = compiledProjectTemplate(this);
+}
+
+function createProjects(projectData){
+  projectData.forEach(function(project){
+    var projectObj = new Project(project);
+    projects.push(projectObj);
+  })
+
+  projects.forEach(function(project){
+    $('.projects').append(project.toHtml());
+  })
+}
+
+function displayProjects(){
+  $.get('data/projects.js', function(response){
+    // localStorage.setItem('rawProject', JSON.stringify(response));
+    createProjects(response);
+  })
+}
