0c7ca009172f21ef735466bd661cac8d26010d10
Shannon Dillon <sedillon93@gmail.com>
2017-10-11 10:17:30 -0700

    set up server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const EXPRESS = require('express');
+const PARSER = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+const APP = EXPRESS();
+
+APP.use(EXPRESS.static('public'));
+
+APP.get('*', function(request, response){
+  response.sendFile('index.html', {root: './public'});
+})
+
+APP.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`)
+})
