dff8b9b78c3e5aa0ca1de2f4145cb08ec1577fb8
Shannon Dillon <sedillon93@gmail.com>
2017-10-13 16:15:03 -0700

    trying to add different paths for server.js to listen to

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ const APP = EXPRESS();
 
 APP.use(EXPRESS.static('public'));
 
+APP.get('/', function(request, response){
+  response.sendFile('index.html', {root: './public'});
+})
+
 APP.get('/projects', function(request, response){
-  response.sendFile('projects.json', {root: './public/data'});
+  response.sendFile('projects.json', {root: './public/data'})
 })
 
 APP.listen(PORT, () => {
