66753342f2d10c6e9392df339dfb2d5a24aa486d
Shannon Dillon <sedillon93@gmail.com>
2017-10-13 17:00:37 -0700

    writing 'createJobs' function to fill and append template to DOM'

--- public/scripts/model/jobs.js
+++ public/scripts/model/jobs.js
@@ -1,8 +1,18 @@
 'use strict';
 
-function Job(){
-  title:
-  employer:
-  description:
-  dates:
+function Job(jobData){
+  this.title: jobData.title,
+  this.employer: jobData.employer,
+  this.description: jobData.description,
+  this.dates: jobData.dates
+}
+
+Job.prototype.toHTML(){
+  let jobHTML = $('#job_template').html();
+  let fillJobTemplate = Handlebars.compile(jobHTMl);
+  return fillJobTemplate(this);
+}
+
+function createJobs(){
+
 }
