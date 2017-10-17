c31dbebe4182e780e15d8c64aaeecda662e1b2a6
Shannon Dillon <sedillon93@gmail.com>
2017-10-17 11:33:40 -0700

    actually finish putting all functions in IIFEs

--- public/scripts/model/projects.js
+++ public/scripts/model/projects.js
@@ -1,43 +1,47 @@
 'use strict';
 
+var portfolio = portfolio || {};
 const projects = [];
 
-function Project(projectData){
-  this.title = projectData.title,
-  this.description = projectData.description,
-  this.languages = projectData.languages,
-  this.dates = projectData.dates,
-  this.link = projectData.link
-}
+(function(module){
+  function Project(projectData){
+    this.title = projectData.title,
+    this.description = projectData.description,
+    this.languages = projectData.languages,
+    this.dates = projectData.dates,
+    this.link = projectData.link
+  }
 
-Project.prototype.toHtml = function(){
-  let projectTemplateHTML = $('#project_template').html();
-  let fillProjectTemplate = Handlebars.compile(projectTemplateHTML);
-  return fillProjectTemplate(this);
-}
+  Project.prototype.toHtml = function(){
+    let projectTemplateHTML = $('#project_template').html();
+    let fillProjectTemplate = Handlebars.compile(projectTemplateHTML);
+    return fillProjectTemplate(this);
+  }
 
-function createProjects(projectData){
-  projectData.forEach(function(project){
-    var projectObj = new Project(project);
-    projects.push(projectObj);
-  })
+  Project.createProjects = function(projectData){
+    projectData.forEach(function(project){
+      var projectObj = new Project(project);
+      projects.push(projectObj);
+    })
 
-  projects.forEach(function(project){
-    $('.projects').append(project.toHtml());
-  })
-}
+    projects.forEach(function(project){
+      $('.projects').append(project.toHtml());
+    })
+  }
 
-function displayProjects(){
-  if (!localStorage.projectData){
-    $.getJSON('/data/projects.json')
+  Project.displayProjects = function(){
+    if (!localStorage.projectData){
+      $.getJSON('/data/projects.json')
       .done(function(response){
         localStorage.setItem('projectData', JSON.stringify(response));
         if ($('.projects').is(':empty')){
-          createProjects(response)
+          Project.createProjects(response)
         }
       });
+    }
+    else if ($('.projects').is(':empty')){
+      Project.createProjects(JSON.parse(localStorage.projectData));
+    }
   }
-  else if ($('.projects').is(':empty')){
-    createProjects(JSON.parse(localStorage.projectData));
-  }
-}
+  module.Project = Project;
+})(portfolio)
