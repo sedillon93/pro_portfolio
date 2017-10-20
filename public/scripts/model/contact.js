c462f76a8ac84ab483eb43e40efe71322b5760ba
Shannon Dillon <sedillon93@gmail.com>
2017-10-20 16:50:49 -0700

    fix footer positioning; fix duplicate appending of contact info

--- public/scripts/model/contact.js
+++ public/scripts/model/contact.js
@@ -7,7 +7,9 @@ var portfolio = portfolio || {};
   Contacts.createContacts = function(){
     let contactHTML = $('#contact_template').html();
     let fillContactTemplate = Handlebars.compile(contactHTML);
-    $('.contact').append(fillContactTemplate({email: "sedillon93@gmail.com", phone: "215-806-8039"}));
+    if ($('.contact').is(':empty')) {
+      $('.contact').append(fillContactTemplate({email: 'sedillon93@gmail.com', phone: '215-806-8039'}));
+    }
   }
   module.Contacts = Contacts;
 })(portfolio)
